94fa6aca328ba8882aeca9a31a6ca3ccbf1b0928
Ivana Escobar <ivana@oden.utexas.edu>
2021-08-11 13:32:02 -0500

    make repo into python package

---
+++ setup.py
@@ -0,0 +1,22 @@
+ from setuptools import setup
+ 
+ setup(
+   name='sandbox',
+   version='0.0',
+   packages=['sandbox', 'sandbox.test'],
+   url='https://github.com/IvanaEscobar/sandbox',
+   keywords=['ocean','computation'],
+   license='LICENSE.txt',
+   description='Rough Draft Code Package',
+   long_description=open('README.txt').read(),
+   long_description_content_type='text/markdown'
+   install_requires=[
+       "numpy >= 1.19.1",
+       "pandas",
+       "xarray",
+       "xmitgcm",
+       "ecco_v4_py"
+   ],
+   author='Ivana Escobar',
+   author_email='ivana@utexas.edu'
+)
