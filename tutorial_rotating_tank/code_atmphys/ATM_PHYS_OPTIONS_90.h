81fe64f9f39c8b92bef19e8c057b8cf6733fc065
Ivana Escobar <ivana@utexas.edu>
2022-01-26 11:57:30 -0600

    atm_phys package updates to be able to use eesupp macros

---
+++ tutorial_rotating_tank/code_atmphys/ATM_PHYS_OPTIONS_90.h
@@ -0,0 +1,10 @@
+#ifndef ATM_PHYS_OPTIONS_90_H
+#define ATM_PHYS_OPTIONS_90_H
+#include "PACKAGES_CONFIG.h"
+#include "CPP_OPTIONS_90.h"
+
+#ifdef ALLOW_ATM_PHYS
+!C     Package-specific options go here
+
+#endif /* ALLOW_ATM_PHYS */
+#endif /* ATM_PHYS_OPTIONS_90_H */
