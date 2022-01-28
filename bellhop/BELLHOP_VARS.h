32688d6284d6bfc784271a8040edba5d4816b636
Ivana Escobar <ivana@utexas.edu>
2022-01-28 14:54:36 -0600

    successful compilation and run with tutorial_global_oce_latlon

--- bellhop/BELLHOP_VARS.h
+++ bellhop/BELLHOP_VARS.h
@@ -2,20 +2,20 @@
 
 #ifdef BELLHOP_3D_STATE
 C     Bellhop 3-dim. fields
-      _RL myPa_StatScal1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_StatScal2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_StatVelU(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_StatVelV(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /MYPA_STATE_3D/
-     &    myPa_StatScal1, myPa_StatScal2,
-     &    myPa_StatVelU,  myPa_StatVelV
+      _RL myPa_VarStatScal1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL myPa_VarStatScal2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL myPa_VarStatVelU(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      _RL myPa_VarStatVelV(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
+      COMMON /BELLHOP_STATE_3D/
+     &    myPa_VarStatScal1, myPa_VarStatScal2,
+     &    myPa_VarStatVelU,  myPa_VarStatVelV
 #endif /* BELLHOP_3D_STATE */
 #ifdef BELLHOP_2D_STATE
 C     Bellhop 2-dim. fields
       _RL bellhop_SST (1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL myPa_Surf2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      COMMON /MYPA_STATE_2D/
-     &    bellhop_SST, myPa_Surf2
+      _RL myPa_VarSurf2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
+      COMMON /BELLHOP_STATE_2D/
+     &    bellhop_SST, myPa_VarSurf2
 #endif /* BELLHOP_2D_STATE */
 
 #endif /* ALLOW_BELLHOP */
