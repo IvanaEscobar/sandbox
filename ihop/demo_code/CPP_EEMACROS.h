a4c29d3eaeb38ac3eefc4fb778d24e510bce3835
Ivana Escobar <ivana@utexas.edu>
2022-06-14 17:03:35 -0500

    removed bellhop named function and added a newer checkpoint chunk in cpp-eemacros.h

--- ihop/demo_code/CPP_EEMACROS.h
+++ ihop/demo_code/CPP_EEMACROS.h
@@ -196,4 +196,12 @@
 !C     and S/R open_copy_data_file.
 #define FMT_TSK_ID 'I6.6'
 
+C--   Set ACTION= in OPEN instruction for input file (before doing IO)
+C     leave it empty (if EXCLUDE_OPEN_ACTION) or set it to proper value
+#ifdef EXCLUDE_OPEN_ACTION
+# define _READONLY_ACTION
+#else
+# define _READONLY_ACTION ACTION='read',
+#endif
+
 #endif /* _CPP_EEMACROS_H_ */
