c596f0eabd92b4f24a577e5d4e546fadce1980fa
Ivana Escobar <ivana@oden.utexas.edu>
2022-08-09 11:47:24 -0500

    remove redundant directories; store jupnb in bin directory

--- sandbox/mitprof/__init__.py
+++
@@ -1,9 +0,0 @@
-from .utils import (
-        readOOI
-        )
-from .mitprof import (
-        MITprof_read
-        )
-
-__all__ = [ 'mitprof',
-            'utils']
--- sandbox/ooi_pioneer/__init__.py
+++
@@ -1,9 +0,0 @@
-from .utils import (
-        readOOI
-        )
-from .download_OOIdata import (
-        load_data
-        )
-
-__all__ = [ 'download_OOIdata',
-            'utils']
