da2b6962322d9b2f2271758ddc1b5817b3514aca
Ivana Escobar <ivana@utexas.edu>
2022-08-31 14:03:14 -0500

    great circle distance for curvilinear grid

--- sandbox/__init__.py
+++ sandbox/__init__.py
@@ -1,6 +1,7 @@
 from .utils import (
         loadMatFile,
         degMinSec2decimal,
+        gcDistance,
         wgs84Distance
         )
 from .unesco import (
--- sandbox/utils.py
+++ sandbox/utils.py
@@ -1,5 +1,5 @@
 from scipy.io import loadmat
-from numpy import abs, cos, sqrt
+from numpy import abs, cos, sqrt, arctan, sin, arccos, pi
 
 def loadMatFile ( file ):
     return loadmat(file)
@@ -7,8 +7,26 @@ def loadMatFile ( file ):
 def degMinSec2decimal (degree, minutes=0.0, seconds=0.0):
     return degree + minutes/60 + seconds/3600
 
+def gcDistance( latA, lonA, latB, lonB ):
+# Great circle distance is the shortest distance between two points on a sphere
+# Using the Vicenty formula for an ellipsoid with equal major and minor axes
+# Lats and lons are in degrees
+# Distance is in meters
+    radius = 6371009.0 # [m] mean Earth radius
+    latA = latA * pi/180.
+    latB = latB * pi/180.
+    dlon = abs(lonB - lonA) * pi/180. 
+
+    numer = ( cos(latB)*sin(dlon) )**2 + \
+            ( cos(latA)*sin(latB) - sin(latA)*cos(latB)*cos(dlon) )**2
+    denom = sin(latA)*sin(latB) + cos(latA)*cos(latB)*cos(dlon)
+
+    return radius * arctan( sqrt(numer) / denom )
+
 def wgs84Distance( latA, lonA, latB, lonB ):
 # For the WGS84 spheroid up to 3 terms in lat and 2 terms in lon
+# Lats and lons are in degrees
+# Distance is in meters
     midLat = (latA + latB) / 2.
     dlat = abs(latB - latA)
     dlon = abs(lonB - lonA) 
