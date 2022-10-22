28934affda05be1daa6d7e5e2d6ecd3be006b125
Ivana Escobar <ivana@utexas.edu>
2022-10-22 10:41:03 -0500

    coords for experiments

--- sandbox/nesba.py
+++ sandbox/nesba.py
@@ -6,8 +6,8 @@ CTD_coords = {'OSPM': [-70.88033, 39.93556, "#1772b1"], \
               'PMCO': [-70.88768, 40.10123, "#249c24"], \
               'PMCI': [-70.88893, 40.22695, "#d62728"]    }
 
-nesba_coords = {'tm4': [-sb.degMinSec2decimal(70,54.0876), sb.degMinSec2decimal(39,50.8542), 'm'], \
-                'at' : [-sb.degMinSec2decimal(70,53.1670), sb.degMinSec2decimal(39,57.1500), 'r']}
+nesba_coords = {'tm4': [-degMinSec2decimal(70,54.0876), degMinSec2decimal(39,50.8542), 'm'], \
+                'at' : [-degMinSec2decimal(70,53.1670), degMinSec2decimal(39,57.1500), 'r']}
 
 tm4 = [-degMinSec2decimal(70,54.0876), degMinSec2decimal(39,50.8542) ]
 at  = [-degMinSec2decimal(70,53.1670), degMinSec2decimal(39,57.1500) ]
