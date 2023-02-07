62fa9bf542aa581b14ebbed2964d961046cfc7a9
Ivana Escobar <ivana@utexas.edu>
2023-02-07 10:00:36 -0600

    nesba domain reduction by 60.35%

--- sandbox/nesba.py
+++ sandbox/nesba.py
@@ -21,7 +21,42 @@ nesbaDist = wgs84Distance(at[0],  at[1],
                          tm4[0], tm4[1]) / 1000.
 
 #             'name': [XC:lon,    YC:lat,   color]
-gcm_coords = {'nw': [-75.531,   41.753, "k"], \
+gcm_coords_0 = {'nw': [-75.531,   41.753, "k"], \
               'ne': [-69.906,   41.753, "k"], \
               'se': [-69.906,   37.750, "k"], \
               'sw': [-75.531,   37.750, "k"]    }
+
+gcm_coords = {'nw': [-74.5935,   41.7530, "k"], \
+              'ne': [-69.8023,   41.7530, "k"], \
+              'se': [-69.8023,   39.2083, "k"], \
+              'sw': [-74.5935,   39.2083, "k"]    }
+## Using KDtree searching to take llc4320 face 10 and find global indices of 
+## gcm_coords.
+##from scipy.spatial import cKDTree
+##Tile10 = np.stack([ llc4320.XC.values.flatten(), \
+##                    llc4320.YC.values.flatten() ]).T
+##
+##J=[]; I=[]
+##for key in sb.gcm_coords:
+##    _, ii = cKDTree(Tile10).query([ sb.gcm_coords[key][0],\
+##                                    sb.gcm_coords[key][1] ], k=1)
+##    print(Tile10[ii])            
+##    ids = np.unravel_index(ii, llc4320.XC.shape)
+##
+##    J.append( ids[1] )
+##    I.append( ids[2] )
+##                                
+##print('\tnw,   ne,   se,   sw')
+##print("J = ", J,'\nI = ',I) 
+##
+##def renumberLocalIndex (ds):
+##    idxDict = {"i":"I", "j":"J", "i_g":"I_g", "j_g":"J_g"}
+##            
+##    for key in idxDict:
+##        ds[idxDict[key]] = xr.DataArray(ds[key].values, dims=[key],
+##                                        attrs={'long_name':'Global llc index'})
+##        ds[key] = np.arange(len(ds[key]))
+##    return ds
+##nesbaGRID = llc4320.isel(j=slice(min(J), max(J)), i=slice(min(I), max(I)), \
+##                         j_g=slice(min(J),max(J)), i_g=slice(min(I),max(I)))
+##renumberLocalIndex(nesbaGRID)
