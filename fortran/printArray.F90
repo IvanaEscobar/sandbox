3062704806c8bf9b6aff1771e37f686664b4667d
Ivana Escobar <ivana@utexas.edu>
2023-05-08 12:53:01 -0500

    dynamic allocation

--- fortran/printArray.F90
+++ fortran/printArray.F90
@@ -8,14 +8,24 @@
         
         ! Arrays
         REAL(dp), allocatable :: arr(:)
+        REAL(dp) :: IHOP_arr(1)
         INTEGER i 
 
+        DO i=1,1
+            IHOP_arr(i) = 1+i
+        ENDDO
+        arr = IHOP_arr
+
+        !ALLOCATE( arr(MAX(3,1)), Stat=i )
+        if (allocated(arr)) then
+            WRITE(*,*) arr
+        end if
         ! DO i=1,10
         !     arr(i) = i**2
         ! ENDDO
 
-        arr = [(i**2, i=1,10)]
-        arr(1:3) = 500
+        !arr = [(i**2, i=1,10)]
+        !arr(1:3) = 500
       
         WRITE(*,*) "Array = ", arr
       END PROGRAM  writeArray
