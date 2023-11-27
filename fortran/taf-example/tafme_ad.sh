0afd2a17518144c00fba2776e5d451d241c1d47f
Ivana Escobar <ivana@utexas.edu>
2023-11-27 17:00:34 -0600

    simple taf tl and ad

---
+++ fortran/taf-example/tafme_ad.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+staf -toplevel f -input x -output y -reverse -arglist f.f90
+
+# remove the second program...
+# Exit as soon as the first instance of "program" is encountered
+awk '/program/ { exit } { print }' "f_ad.f90" > "f_ad.tmp"
+mv f_ad.tmp f_ad.f90
+
+# compile the code
+gfortran -o tafme_ad drive_f_ad.f90 f_ad.f90
+
+echo 'Input: a, x'
+./tafme_ad
