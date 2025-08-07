aa9702b8328af572ee20a6626773da973f2225b4
Bunny Quesobar <ivana@utexas.edu>
2025-08-07 13:18:12 -0500

    save ihop acoustic domain info

---
+++ sandbox/bcg/bcg.py
@@ -0,0 +1,21 @@
+#!/bin/bash python
+from ..utils import wgs84fromBreaing, wgs84space
+
+ts080 = list(range(21168000, 21189601, 30))
+ts100 = list(range(26337600, 26359201, 30))
+
+# Distance in meters and bearing in degrees
+distance = 400000  # 400 km
+bearing  = 0       # 0 degrees (northwards)
+line_M080 = wgs84fromBearing([14, 47], bearing, distance, npts=25)
+line_M100 = wgs84fromBearing([18, 47], bearing, distance, npts=25)
+
+line_Z080 = wgs84space([16, 56], [21.597, 56], npts=35)
+line_Z100 = wgs84space([16, 59], [22.073, 59], npts=35)
+
+for at in range(len(line_Z080.lats)):
+    line_Z080.lats[at]=56
+    line_Z100.lats[at]=59
+
+lineSource = lambda ds, line: ds.sel(YC=line.lats[0],  XC=line.lons[0],  method='nearest')
+lineReceiv = lambda ds, line: ds.sel(YC=line.lats[-1], XC=line.lons[-1], method='nearest')
